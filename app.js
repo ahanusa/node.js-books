ffb559182a77fccf340f9019591aa12bd5ea0b14
ahanusa <ahanusa@gettyimages.com>
2016-01-21 08:48:53 -0600

    Add support to serve up static files (html, css, etc.)

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(request, response) {
   response.send('Hello World');
 });
