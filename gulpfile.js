54b394231a6df3e2abcf3e83588177649e37f21f
ahanusa <ahanusa@gettyimages.com>
2016-01-22 08:31:29 -0600

    pipe to jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var jsFiles = ['*.js', 'src/**/*.js'];
 var jsHint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 gulp.task('style', function() {
-  gulp.src(jsFiles)
-      .pipe(jsHint())
-      .pipe(jsHint.reporter('jshint-stylish', {
-        verbose: true
-      }));
+  return gulp.src(jsFiles)
+             .pipe(jsHint())
+             .pipe(jsHint.reporter('jshint-stylish', {
+               verbose: true
+             }))
+             .pipe(jscs());
 });
