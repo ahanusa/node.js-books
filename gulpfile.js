972e58968ac697860f154090cbd66df6e061a304
ahanusa <ahanusa@gettyimages.com>
2016-01-22 08:22:50 -0600

    install jshint via 'npm install --save-dev jshint'

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var jsHint = require('gulp-jshint');
 
 gulp.task('style', function() {
   gulp.src(jsFiles)
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish', {
+      .pipe(jsHint())
+      .pipe(jsHint.reporter('jshint-stylish', {
         verbose: true
       }));
 });
