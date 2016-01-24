a559fcf8e73accab44effe297fdf46f967b845c3
ahanusa <ahanusa@gettyimages.com>
2016-01-24 08:26:44 -0600

    add books.ejs and supply with sample data

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ var express = require('express');
 var app = express();
 var port = 5000;
 var bookRouter = express.Router();
+var books = [
+    {
+      title: 'War and peace',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace II',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace III',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace IV',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+];
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -9,8 +35,21 @@ app.set('view engine', 'ejs');
 
 bookRouter.route('/')
           .get(function(request, response) {
-            response.send('Hello Books');
-          });
+            response.render('books',
+                  {
+                    title: 'Books', 
+                    nav: [
+                    {
+                      link: '/books',
+                      text: 'Books'
+                    },
+                    {
+                      link: '/authors',
+                      text: 'Authors'
+                    }
+                  ],
+                  books: books});
+});
 
 bookRouter.route('/single')
           .get(function(request, response) {
@@ -22,7 +61,7 @@ app.use('/books', bookRouter);
 app.get('/', function(request, response) {
   response.render('index', 
                   {
-                    title: 'Hello from render', 
+                    title: 'Index', 
                     nav: [
                     {
                       link: '/books',
