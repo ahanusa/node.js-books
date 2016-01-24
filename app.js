d249cf6b705783ca7e0fa1e8dcb1d54fde0efbff
ahanusa <ahanusa@gettyimages.com>
2016-01-24 07:55:18 -0600

    add dynamic books and author links

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var port = 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.render('index', 
+                  {
+                    title: 'Hello from render', 
+                    nav: [
+                    {
+                      link: '/books',
+                      text: 'Books'
+                    },
+                    {
+                      link: '/authors',
+                      text: 'Authors'
+                    }
+                  ]});
 });
 
 app.get('/books', function(request, response) {
