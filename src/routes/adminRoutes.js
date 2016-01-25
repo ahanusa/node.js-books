fd7e66411c949a5e46a50fa70867cdfefe21cb92
ahanusa <ahanusa@gettyimages.com>
2016-01-24 18:44:12 -0600

    add admin route to add a list of books in mongodb

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [
+    {
+      title: 'War and peace',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace II',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace III',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+    {
+      title: 'War and peace IV',
+      genre: 'Historical Fiction',
+      author: 'Foo Jones',
+      read: false
+    },
+];
 
 var router = function(nav) {
   adminRouter.route('/addBooks')
              .get(function(request, response) {
-               response.send('add books');
-  });
+               var url = 'mongodb://localhost:27017/libraryApp';
+               mongodb.connect(url, function(error, db) {
+                 var collection = db.collection('books');
+                 collection.insertMany(books, function(error, results) {
+                   response.send(results);
+                   db.close();
+                 });
+               });
+             });
 
   return adminRouter;
 };
