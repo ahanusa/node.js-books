0c8a05c3b43dac128744fca609d688b710053b09
ahanusa <ahanusa@gettyimages.com>
2016-01-25 18:00:59 -0600

    Complete book page with data from mongo

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,35 +1,44 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function(nav) {
   bookRouter.route('/')
   .get(function(request, response) {
-     var url = 'mongodb://localhost:27017/libraryApp';
-     mongodb.connect(url, function(error, db) {
-       var collection = db.collection('books');
-       collection.find({}).toArray(function (error, result) {
-          response.render('books',
-                          {
-                            title: 'Books', 
-                            nav: nav,
-                            books: result
-                          });
-          db.close();
-       });
-     });
+    var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(error, db) {
+      var collection = db.collection('books');
+      collection.find({}).toArray(function (error, result) {
+        response.render('books',
+                        {
+                          title: 'Books', 
+                          nav: nav,
+                          books: result
+                        });
+                        db.close();
+      });
+    });
   });
 
-  //bookRouter.route('/:id')
-  //.get(function(request, response) {
-    //var id = request.params.id;
-    //response.render('book',
-                    //{
-                      //title: 'Books', 
-                      //nav: nav,
-                      //book: books[id]
-                    //});
-  //});
+  bookRouter.route('/:id')
+  .get(function(request, response) {
+    var id = new objectId(request.params.id);
+    var url = 'mongodb://localhost:27017/libraryApp';
+    mongodb.connect(url, function(error, db) {
+      var collection = db.collection('books');
+      collection.findOne({_id: id}, function(error, result) {
+        console.log("result", result);
+        response.render('book',
+                        {
+                          title: 'Books', 
+                          nav: nav,
+                          book: result
+                        });
+                        db.close();
+      });
+    });
+  });
 
   return bookRouter;
 };
